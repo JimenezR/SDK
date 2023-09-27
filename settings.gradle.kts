876119269e2fa350e7ba3c95a23894ed122a44ca
Uriel Tovar <ult_18@me.com>
2023-09-27 14:26:17 -0600

    Create project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Platform-Sdk"
+include(":app")
+include(":platform:sdk")
